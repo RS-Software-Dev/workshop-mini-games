417b7ada365ca6fa06f46308db21ade5e4ac1ae4
Roman Schulte <info@roman-schulte.de>
2025-10-06 15:26:44 +0200

    Simplified setup code.

--- examples/scripts/CanvasRenderer.js
+++ examples/scripts/CanvasRenderer.js
@@ -1,66 +1,39 @@
 class CanvasRenderer {
-    constructor({ canvas, tileSize }) {
+    constructor({ canvas, tileSize, draw }) {
         this.canvas = canvas
         this.tileSize = tileSize
         this.context = canvas.getContext('2d')
+        this.draw = draw.bind(this.context)
+
+
+        this.context.fillRoundRect = function(x, y, w, h, r) {
+            this.beginPath()
+            this.roundRect(x, y, w, h, r)
+            this.fill()
+            this.closePath()
+        }.bind(this.context)
     }
 
-    centerInCell({ row, col, draw }) {
-        this.context.save()
-        this.context.translate(
-            row * this.tileSize + this.tileSize / 2,
-            col * this.tileSize + this.tileSize / 2
-        )
-
-        draw()
-        this.context.restore()
-    }
-
-    fillQuad(size) {
-        this.context.beginPath()
-        this.context.roundRect(-size / 2, -size / 2, size, size, size / 4)
-        this.context.fill()
-        this.context.closePath()
-    }
-
-    render(grid) {
+    render(state) {
         this.context.reset()
-        const style = getComputedStyle(this.canvas)
-        const colorAlive = style.getPropertyValue('--color-alive')
-        const colorDead = style.getPropertyValue('--color-dead')
-
-        for (const { row, col, val } of grid.cells()) {
-            this.centerInCell({
-                row,
-                col,
-                draw: () => {
-
-                    if (val) {
-                        this.context.fillStyle = colorAlive
-                    } else {
-                        this.context.fillStyle = colorDead
-                    }
-
-                    this.fillQuad(this.tileSize - 2)
-                }
-            })
-        }
+        this.draw(state)
     }
 }
 
-CanvasRenderer.auto = ({ rows, cols, tileSize }) => {
+CanvasRenderer.auto = ({ rows, cols, tileSize, draw }) => {
     const canvas = document.createElement('canvas')
-    canvas.width = rows * tileSize
-    canvas.height = cols * tileSize
+    canvas.width = cols * tileSize
+    canvas.height = rows * tileSize
 
     return new CanvasRenderer({
-        canvas: canvas,
-        tileSize: tileSize
+        canvas,
+        tileSize,
+        draw
     })
 }
 
-CanvasRenderer.autoAppend = ({ parent, rows, cols, tileSize }) => {
-    const renderer = CanvasRenderer.auto({ rows, cols, tileSize })
+CanvasRenderer.autoAppend = ({ parent, rows, cols, tileSize, draw }) => {
+    const renderer = CanvasRenderer.auto({ rows, cols, tileSize, draw })
     parent.append(renderer.canvas)
     return renderer
 }
--- examples/scripts/GameGrid.js
+++ examples/scripts/GameGrid.js
@@ -2,33 +2,56 @@ class GameGrid {
     constructor({ rows, cols, cell }) {
         this.rows = rows
         this.cols = cols
-        this.vals = []
+        this.data = []
 
         for (let row = 0; row < rows; row++) {
-            this.vals[row] = [];
+            this.data[row] = [];
             for (let col = 0; col < cols; col++) {
                 // Ask the cellFunction what should be here
-                this.vals[row][col] = cell({ row, col });
+                this.data[row][col] = cell({ row, col });
             }
         }
     }
 
-    getCell({ row, col }) {
+    getData({ row, col }) {
         if (0 <= row && row < this.rows && 0 <= col && col < this.cols) {
-            return this.vals[row][col]
+            return this.data[row][col]
         }
 
         return undefined
     }
 
+    setData({ row, col, data }) {
+        if (0 <= row && row < this.rows && 0 <= col && col < this.cols) {
+            this.data[row][col] = data
+        }
+    }
+
+    setGrid({ row, col, data }) {
+        for(let dr = 0; dr < data.length; dr++) {
+            const dataRow = data[dr]
+            for(let dc = 0; dc < dataRow.length; dc++) {
+                this.setData({
+                    row: row + dr,
+                    col: col + dc,
+                    data: dataRow[dc]
+                })
+            }
+        }
+    }
+
+    getCell({ row, col }) {
+        return new GameCell({
+            grid: this,
+            row,
+            col
+        })
+    }
+
     *cells() {
         for (let row = 0; row < this.rows; row++) {
             for (let col = 0; col < this.cols; col++) {
-                yield {
-                    row,
-                    col,
-                    val: this.getCell({ row, col })
-                }
+                yield this.getCell({ row, col })
             }
         }
     }
@@ -37,12 +60,41 @@ class GameGrid {
         for (let dr = -1; dr <= 1; dr++) {
             for (let dc = -1; dc <= 1; dc++) {
                 if (dr === 0 && dc === 0) continue; // Skip self
-                yield {
+                yield this.getCell({
                     row: row + dr,
-                    col: col + dc,
-                    val: this.getCell({ row: row + dr, col: col + dc })
-                }
+                    col: col + dc
+                })
             }
         }
+    }
+}
+
+class GameCell {
+    constructor({ grid, row, col }) {
+        this.grid = grid
+        this.row = row
+        this.col = col
+    }
+
+    get data() {
+        return this.grid.getData({
+            row: this.row,
+            col: this.col
+        })
+    }
+
+    set(data) {
+        this.grid.setData({
+            row: this.row,
+            col: this.col,
+            data
+        })
+    }
+
+    *neighbors() {
+        return this.grid.neighbors({
+            row: this.row,
+            col: this.col
+        })
     }
 }
